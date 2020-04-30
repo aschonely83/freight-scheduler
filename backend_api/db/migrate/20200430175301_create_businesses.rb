0fa4680cff71546248b9aea07a56ad9350698bda
Aschonely <aschonely@gmail.com>
2020-04-30 15:00:54 -0400

    set up database and migrated

--- backend_api/db/migrate/20200430175301_create_businesses.rb
+++ backend_api/db/migrate/20200430175301_create_businesses.rb
@@ -1,7 +1,10 @@
 class CreateBusinesses < ActiveRecord::Migration[6.0]
   def change
     create_table :businesses do |t|
-      
+      t.string :name
+      t.string :pallets
+      t.string :scheduled_day
+      t.string :confirmation_number
       
       t.timestamps
     end
--- backend_api/db/migrate/20200430175416_create_carriers.rb
+++ backend_api/db/migrate/20200430175416_create_carriers.rb
@@ -1,6 +1,11 @@
 class CreateCarriers < ActiveRecord::Migration[6.0]
   def change
     create_table :carriers do |t|
+      t.string :name
+      t.string :email
+      t.string :confirmation_number
+      t.integer :schedule_id
+      t.integer :business_id
 
       t.timestamps
     end

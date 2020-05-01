a02e6541fd85222a2f6ddf6359a4f18e35a1f8f4
Aschonely <aschonely@gmail.com>
2020-05-01 09:45:41 -0400

    set up index and show method

--- backend_api/app/controllers/businesses_controller.rb
+++ backend_api/app/controllers/businesses_controller.rb
@@ -1,2 +1,11 @@
 class BusinessesController < ApplicationController
+  def index
+    businesses = Business.all 
+    render json: businesses, except: [:created_at, :updated_at]    
+  end
+      
+  def show
+    business = Business.find_by_id(params[:id])
+    render json: business, except: [:created_at, :updated_at]    
+  end
 end

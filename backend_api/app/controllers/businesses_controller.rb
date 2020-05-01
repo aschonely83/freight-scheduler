8ee507ecf980bb82b0fc9847492bb182f925bc08
Aschonely <aschonely@gmail.com>
2020-05-01 12:12:32 -0400

    built out basic business controller actions

--- backend_api/app/controllers/businesses_controller.rb
+++ backend_api/app/controllers/businesses_controller.rb
@@ -10,4 +10,28 @@ class BusinessesController < ApplicationController
     business = Business.find_by_id(params[:id])
     render json: business, except: [:created_at, :updated_at]    
   end
+
+  # POST /businesses
+  def created
+    business = Business.create(business_params)
+    render  json: business, except: [:created_at, :updated_at]  
+  end
+
+  # PATCH/PUT /businesses/1 
+  def updated
+    if business.update(business_params)
+      render json: business
+    else
+      render json: business.error, status: :unprocessable_entity      
+    end
+  end
+  
+  # DELETE /businesses/1
+  def destroy
+    business.destroy
+  end
+
+  def business_params
+    params.require(:businesses).permit( :name, :pallets, :scheduled_day, :confirmation_number)
+  end
 end

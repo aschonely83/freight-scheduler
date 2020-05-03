2ca83623c71aad2223685a9420c08d857175153b
Aschonely <aschonely@gmail.com>
2020-05-03 15:16:29 -0400

    updated methods for both controllers

--- backend_api/app/controllers/businesses_controller.rb
+++ backend_api/app/controllers/businesses_controller.rb
@@ -1,22 +1,28 @@
 class BusinessesController < ApplicationController
-  before_action :set_event, only: [:show, :update, :destroy]
-
+  
   #GET /businesses
     def index
     businesses = Business.all 
-    render json: businesses, except: [:created_at, :updated_at]    
+    render json: BusinessSerializer.new(businesses)    
   end
   
   # GET /businesses/1
   def show
-    business = Business.find_by_id(params[:id])
-    render json: business, except: [:created_at, :updated_at]    
+    business = Business.find_by(id: params[:id])
+    options = {
+      include: [:carriers]
+    }
+    render json: BusinessSerializer.new(business, options)    
   end
 
   # POST /businesses
   def create
-    business = Business.create(business_params)
-    render  json: business, except: [:created_at, :updated_at]  
+    business = Business.new(business_params)
+    if business.save
+    render  json: business, status: accepted
+    else
+      render json: {errors: business.errors.full_messages}, status: :unprocessable_entity
+    end    
   end
 
   # PATCH/PUT /businesses/1 
@@ -39,7 +45,7 @@ class BusinessesController < ApplicationController
   end
 
   def business_params
-    params.require(:business).permit( :name, :pallets, :scheduled_day, :confirmation_number)
+    params.require(:business).permit(:name, :pallets, :scheduled_day, :confirmation_number)
   end
 end
 
--- backend_api/app/controllers/carriers_controller.rb
+++ backend_api/app/controllers/carriers_controller.rb
@@ -1,2 +1,20 @@
 class CarriersController < ApplicationController
+    def index
+      carriers = Carrier.all 
+      render json: CarrierSerializer.new(carriers)    
+    end
+
+    def create
+      carrier = Carrier.new(carrier_params)
+      if carrier.save
+        render json: carrier, status: :accepted
+      else 
+        render json: {errors: carrier.errors.full_messages}, status: :unprocessible_entity   
+      end   
+    end
+
+    private
+    def carrier_params
+        params.require(:carrier).permit(:name, :email, :confirmation_number, :business_id)
+    end
 end

e936cfbfef8a4399e55e6282d16d563508cc281e
Aschonely <aschonely@gmail.com>
2020-05-04 13:29:56 -0400

    built out basic fetch calls in an API class

--- frontend_client/js/index.js
+++ frontend_client/js/index.js
@@ -1,59 +1,80 @@
-BUSINESSES_URL = 'http://localhost:3000/businesses'
-
-const addButton = document.createElement('button')
-addButton.innerText = "Add Business"
-document.body.appendChild(addButton)
-addButton.addEventListener('click', addBusiness)
-
-fetch(BUSINESSES_URL)
-.then(resp => resp.json())
-.then(json => json.forEach(business => {
-  renderBusinesses(business)
- }),
-)
-
-function addBusiness() {
-    let options = {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-        }
-    } 
-    fetch(BUSINESSES_URL, options)
-    .then(resp =>  resp.json())
-    .then(business => {
-        renderBusinesses(business) 
-        addListeners(business) 
-    })
-  
-}
-   
-
-function renderBusinesses(business){
-    let html = `
-    <div data-id="${business.id}">
-      <p>${business.name}</p>
-      <p>${business.pallets}</p>
-      <p>${business.scheduled_day}</p>
-      <p>${business.confirmation_number}</p>
-    </div><br><br>`
-    let divCard = document.createElement('div')
-    divCard.setAttribute("class", "card")
-    divCard.setAttribute("data-card-id", `${business.id}`)
-    divCard.innerHTML = html
-    document.body.appendChild(divCard)
-    }
-
-function addListeners(business){
-    let removeButtons = document.querySelector(`[data-card-id='${business.id}']`)
-    removeButtons.addEventListener('click', function(e) { removeBusiness(e) } )
+class SeventeenerAPI {
+  static getBusiness(){
+    return fetch(`${SeventeenerAPI.BASE_URL}/businesses`).then(resp =>resp,json())
   }
- 
-function removeBusiness(e) { 
-     options = {
-       method: "DELETE"
-     }
-         fetch(`${BUSINESSES_URL}/${e.target.dataset.name}`, options)
-         e.target.parentElement.parentElement.remove()
-}  
+  
+  static getBusinessShow(businessId) {
+    return fetch(`${SeventeenerAPI.BASE_URL}/businesses/${businessId}`)
+    .then(res =>  res.json() )
+    .then(json => {
+    const { 
+        data: {  
+            id,
+            attributes: {
+            name,
+            pallets,
+            scheduled_day,
+            confirmation_number
+            }
+        },
+        included
+        } = json
+        return {
+            id,
+            name,
+            pallets,
+            scheduled_day,
+            confirmation_number,
+            carriers: included.map(({id, attributes: {name, email, confirmation_number, business_id}}) => {
+                return {
+                id,
+                name,
+                email,
+                confirmation_number, 
+                business_id
+                }
+            })
+        }
+        
+    })
+   }
+
+  static createBusiness(businessAttr) {
+    return fetch(`${SeventeenerAPI.BASE_URL}/businesses`, {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(businessAttr)
+    }).then( res =>  res.json() )
+  }
+
+  static createCarrier(carrierAttr) {
+    return fetch(`${SeventeenerAPI.BASE_URL}/carriers`, {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(carrierAttr)
+    }).then( res =>  res.json() )
+  } 
+}
+
+SeventeenerAPI.BASE_URL = "http://localhost:3000"
+
+class Business {
+  constructor(business) {
+    this.id = business.id
+    this.name = business.name
+    this.pallets = business.pallets
+    this.scheduled_day = business.scheduled_day
+    this.confirmation_number = business.confirmation_number
+  }
+}
+
+class Carrier {
+
+}
+

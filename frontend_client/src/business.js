250b5d37221d61e9392e23edb59b63eb756d0cb7
Aschonely <aschonely@gmail.com>
2020-05-08 23:35:29 -0400

    added id to business class

--- frontend_client/src/business.js
+++ frontend_client/src/business.js
@@ -1,9 +1,14 @@
 class Business {
-    constructor(business) {
-        this.id = business.id,
-        this.name = business.name,
-        this.pallets = business.pallets,
-        this.scheduled_day = business.scheduled_day,
-        this.confirmation_number = business.confirmation_number
+    constructor(id, name, pallets, scheduled_day, confirmation_number,carriers) {
+        this.name = name,
+        this.pallets = pallets,
+        this.scheduled_day = scheduled_day,
+        this.confirmation_number = confirmation_number,
+        this.carriers = carriers
+        this.id = id
+        AppContainer.businesses.push(this);
+          
     }
+
+    
 }

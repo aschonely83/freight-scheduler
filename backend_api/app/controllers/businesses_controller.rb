e7c50a65e69904127242b49a875d9e972c498abe
Aschonely <aschonely@gmail.com>
2020-05-08 23:33:42 -0400

    update business controller, added to routes

--- backend_api/app/controllers/businesses_controller.rb
+++ backend_api/app/controllers/businesses_controller.rb
@@ -2,50 +2,14 @@ class BusinessesController < ApplicationController
   
   #GET /businesses
     def index
-    businesses = Business.all 
-    render json: BusinessSerializer.new(businesses)    
-  end
-  
-  # GET /businesses/1
-  def show
-    business = Business.find_by(id: params[:id])
-    options = {
-      include: [:carriers]
-    }
-    render json: BusinessSerializer.new(business, options)    
-  end
-
-  # POST /businesses
-  def create
-    business = Business.new(business_params)
-    if business.save
-    render  json: business, status: accepted
-    else
-      render json: {errors: business.errors.full_messages}, status: :unprocessable_entity
-    end    
-  end
-
-  # PATCH/PUT /businesses/1 
-  def updated
-    if business.update(business_params)
-      render json: business
-    else
-      render json: business.error, status: :unprocessable_entity      
-    end
+      render :json => Business.all, :include => :carriers    
   end
   
   # DELETE /businesses/1
   def destroy
-    business.destroy
+    Business.find(params[:id]).destroy
+    redirect_to '/businesses'
   end
   
-  private
-  def set_business
-    business = Business.find(params[:id])
-  end
-
-  def business_params
-    params.require(:business).permit(:name, :pallets, :scheduled_day, :confirmation_number)
-  end
 end
 
--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :carriers
-  resources :businesses
+  resources :carriers, only: :index 
+  resources :businesses, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

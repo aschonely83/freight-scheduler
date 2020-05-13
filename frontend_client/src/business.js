4644588bbb8f71c6b094e88eccccbbf330c614f6
Aschonely <aschonely@gmail.com>
2020-05-13 10:25:47 -0400

    added id to contructor

--- frontend_client/src/business.js
+++ frontend_client/src/business.js
@@ -1,14 +1,11 @@
 class Business {
-    constructor(id, name, pallets, scheduled_day, confirmation_number,carriers) {
-        this.name = name,
-        this.pallets = pallets,
-        this.scheduled_day = scheduled_day,
-        this.confirmation_number = confirmation_number,
-        this.carriers = carriers
-        this.id = id
-        AppContainer.businesses.push(this);
-          
+    constructor(id, name, pallets, scheduled_day, confirmation_number) {
+    this.id = id,
+    this.name = name,
+    this.pallets = pallets,
+    this.scheduled_day = scheduled_day
+    this.confirmation_number = confirmation_number
+    AppContainer.businesses.push(this)
     }
 
-    
 }

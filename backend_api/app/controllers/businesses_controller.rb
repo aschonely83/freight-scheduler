db1f1ce6806e57acb843c268da4942d48be005c2
Aschonely <aschonely@gmail.com>
2020-05-13 10:21:09 -0400

    reformatted controller to work with the serializer

--- backend_api/app/controllers/businesses_controller.rb
+++ backend_api/app/controllers/businesses_controller.rb
@@ -3,22 +3,18 @@ class BusinessesController < ApplicationController
   #GET /businesses
   def index
     businesses = Business.all
-    options = {
-      include: [:carriers],
-      exclude: [:created_at, :updated_at]
-    }
-    render json: BusinessSerializer.new(businesses, options)
+    render json: BusinessSerializer.new(businesses)
   end
     
   def show
-    business = Business.find(params[:id])
-    render json: BusinessSerializer.new(business) 
+    business = Business.find_by(id: params[:id])
+    render json: BusinessSerializer.new(business)
   end
   
    #create /businesses
   def create
     business = Business.create(name: params[:name], pallets: params[:pallets], scheduled_day: params[:scheduled_day], 
     confirmation_number: params[:confirmation_number])
-    render json: BusinessSerializer.new(businesses)
+    render json: business, status: 200
   end 
 end     

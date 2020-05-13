b442ba843415bf32c97b1cdf12b50c9df36a7af7
Aschonely <aschonely@gmail.com>
2020-05-13 10:22:54 -0400

    updated index controller based on using serializer

--- backend_api/app/controllers/carriers_controller.rb
+++ backend_api/app/controllers/carriers_controller.rb
@@ -1,13 +1,25 @@
 class CarriersController < ApplicationController
   def index
     carriers = Carrier.all
-    options = {
-      exclude: [:created_at, :updated_at]
-    }
-    render json: CarrierSerializer.new(carriers, options)
+    render json: CarrierSerializer.new(carriers)
   end
 
-    def create
-      
-    end
+  def show
+    carrier = Carrier.find_by(id: params[:id])
+    render json: CarrierSerializer.new(carrier)
+  end
+  
+  def create
+    carrier = Carrier.new(carrier_params)
+    if carrier.save
+      render json: CarrierSerializer.new(carrier)
+    end        
+  end
+
+  private
+
+  def carrier_params
+    params.require(:carrier).permit(:name, :email, :confirmation_number, :business_id)
+  end
+
 end

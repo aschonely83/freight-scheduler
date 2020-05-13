c9e0c3022b0ab44dc80563fa6687b9cedf4db2c5
Aschonely <aschonely@gmail.com>
2020-05-13 10:28:12 -0400

    updated contructor arguments for carrier

--- frontend_client/src/carrier.js
+++ frontend_client/src/carrier.js
@@ -1,9 +1,9 @@
 class Carrier {
-    constructor(id, name, email, confirmation_number, business_id ) {
-        this.id = id,
+    constructor(name, email, confirmation_number, busiess_id) {
         this.name = name,
         this.email = email,
         this.confirmation_number = confirmation_number,
-        this.business_id = business_id
+        this.business_id = busiess_id 
+        AppContainer.carriers.push(this) 
     }
 }

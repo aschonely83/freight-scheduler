5153c8405a21643d6e74144a953007c1878ecc36
Aschonely <aschonely@gmail.com>
2020-05-14 13:32:55 -0400

    nested routes

--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :carriers 
-  resources :businesses
+  resources :businesses, except: [:new] do 
+    resources :carriers, only: [:show, :create] 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

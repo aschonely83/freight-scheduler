94b2dcbc75e9c13dbf55c582810c8bda869954c1
Aschonely <aschonely@gmail.com>
2020-05-14 13:34:02 -0400

    add delete for business, create for carrier

--- frontend_client/src/appContainer.js
+++ frontend_client/src/appContainer.js
@@ -7,9 +7,34 @@ class AppContainer  {
      bindEventListeners() { 
        const newBusinessForm = document.getElementById("createBusinessForm")
        newBusinessForm.addEventListener("submit", this.createBusiness.bind(this));
+      
+       document.addEventListener('click', e => {
+          const addCarrButton = document.getElementById("addCarrier")
+           if (e.target == addCarrButton) {
+             document.getElementById("carrierForm").style.display = "block"
+           }
+       })
+
+       document.addEventListener('click', e => {
+        const deleteButton = document.getElementById("deleteBusiness")
+         if (e.target == deleteButton) {
+             this.destroyBusiness(deleteButton.parentElement.dataset.id)
+             deleteButton.parentElement.remove()
+         }
+     })
+        const carrierForm = document.getElementById("carrierForm")
+        carrierForm.addEventListener('submit', e => {
+          const addCarrierButton = document.getElementById("addCarrier")
+          e.preventDefault()
          
-    // const dlb = document.getElementById("deleteBusiness")
-     // dlb.addEventListener("click", this.deleteBusiness);
+          this.createCarriers(addCarrierButton.parentElement.dataset.id, e)
+        }) 
+       
+       //const addCarrierButton = document.getElementById("addCarrier")
+       //addCarrierButton.addEventListener("click", this.createCarrierFields.bind(this));
+
+       //const dlb = document.getElementById("deleteBusiness")
+       //dlb.addEventListener("click", this.deleteBusiness);
      };
      
      // fetch request
@@ -18,9 +43,10 @@ class AppContainer  {
         .then(resp => resp.json())
         .then(businesses => {
          
-          businesses.data.forEach(business => { 
-            const newBusiness = new Business(business.id, business.attributes.name, business.attributes.pallets, 
+          businesses.data.forEach(business => {
+              new Business(business.id, business.attributes.name, business.attributes.pallets, 
               business.attributes.scheduled_day, business.attributes.confirmation_number, business.attributes.carriers)
+            
               this.renderBusinesses();                   
           });
           
@@ -36,21 +62,26 @@ class AppContainer  {
       const p1 = document.createElement('p')
       const p2 = document.createElement('p')
       const p3 = document.createElement('p')
+      const deleteBusiness = document.createElement('button')
+      const addCarrier = document.createElement('button')
       const main = document.createElement('main')
+     // const carrForm =  
+      
       
       
       AppContainer.businesses.forEach(business => {
        
-        main.append(div)
+       
         div.append(div1)
         div1.append(p)
         div1.append(p1)
         div1.append(p2)
         div1.append(p3)
+       
         
-                        
+
         div.setAttribute('class', "card")
-        div.setAttribute('id', "card")
+        div.setAttribute('data-id', `${business.id}`)
         div1.setAttribute('class',"card-body")
         p.setAttribute('class', "business-name")
         p.textContent =  business.name,  
@@ -60,7 +91,14 @@ class AppContainer  {
         p2.textContent = business.scheduled_day;
         p3.setAttribute('class', "business-confirm-number")
         p3.textContent = business.confirmation_number;
-                              
+        deleteBusiness.setAttribute('id', "deleteBusiness")
+        deleteBusiness.textContent = 'Delete Business'
+       
+        addCarrier.setAttribute('id', "addCarrier")
+        addCarrier.textContent = 'Add Carrier'
+        div.append(deleteBusiness)
+        div.append(addCarrier)
+        main.append(div)
       })
       
       document.body.append(div) 
@@ -86,10 +124,36 @@ class AppContainer  {
       })
       .then(resp => resp.json())
       .then(data => data )
+      .then(location.reload())
       .catch(err => alert(err));
     }
+
+    destroyBusiness(id) {
+      
+      
+      fetch(`${this.url}/businesses/${id}`, {
+        method: "DELETE"
+       })
+       .then(location.reload())
+    }
   
-  
+   createCarriers(id, e) {
+     fetch(`${this.url}/businesses/${id}/carriers`, {
+       method: "POST",
+       headers: {
+         "Content-Type": 'application/json',
+         "Accept": 'application/json'
+       },
+       body: JSON.stringify({
+         name: e.target.name.value,
+         email: e.target.email.value,
+         confirmation_number: e.target.confirmationnumber.value
+       })
+     })
+     .then(resp => resp.json())
+     .then(data => data)
+     .then(location.reload())
+   }
     
   
 }    

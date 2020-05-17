3a0847fa93b30f9b747a790ca1cb13bc0aa03e6a
Aschonely <aschonely@gmail.com>
2020-05-17 19:23:02 -0400

    added index to carriers routes

--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :businesses, except: [:new] do 
-    resources :carriers, only: [:show, :create] 
+    resources :carriers, only: [:index, :show, :create] 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
